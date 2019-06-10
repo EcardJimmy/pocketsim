7e8f85b2cff8066c1a4ce98e11af676c67938959
John Allwine <john@pocketnc.com>
2019-06-10 14:57:55 -0600

    build of fc2b8e01e2f71b363c4b13d4dea5ebb1d394239c v0.3.0

---
+++ precache-manifest.4510f83dbd3d86238feffe9e64971e62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ccc0a6d53a3aa4eef356e57e49dfde",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f6515f5cf0ffa60e3433",
+    "url": "./static/css/main.79a8f2b7.chunk.css"
+  },
+  {
+    "revision": "34c1875b7c71398eef62",
+    "url": "./static/js/2.ffe8a843.chunk.js"
+  },
+  {
+    "revision": "f6515f5cf0ffa60e3433",
+    "url": "./static/js/main.c7b68148.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "2eac3182d4bc82df0df228be9c696dc3",
+    "url": "./static/media/pocketnc.2eac3182.png"
+  }
+]);
