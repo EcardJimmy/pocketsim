3a885821cebcfd899eaef737db484c7b9d5c970e
John Allwine <john@pocketnc.com>
2020-08-26 16:14:58 -0600

    build of efb5124973eb573c9876ec947e778c9b8b345db4 v0.4.0

---
+++ precache-manifest.1a563f4eb0c64ac672c48350f2ac997c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f07c72ef4ac91b49fbfdb5b6479904c6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c7fd4462a5d1a3b807e8",
+    "url": "./static/css/main.79a8f2b7.chunk.css"
+  },
+  {
+    "revision": "a5a52a48b124a760bf8b",
+    "url": "./static/js/2.f55fe824.chunk.js"
+  },
+  {
+    "revision": "09f71d42d912e0d78942f06289bd0a38",
+    "url": "./static/js/2.f55fe824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7fd4462a5d1a3b807e8",
+    "url": "./static/js/main.47de79cd.chunk.js"
+  },
+  {
+    "revision": "16e6a01527bd9dba7dd0",
+    "url": "./static/js/runtime-main.c267b696.js"
+  },
+  {
+    "revision": "2eac3182d4bc82df0df228be9c696dc3",
+    "url": "./static/media/pocketnc.2eac3182.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ba570b3adba8c126bc8d6d4cfb51c393.js"
+  "./precache-manifest.1a563f4eb0c64ac672c48350f2ac997c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
